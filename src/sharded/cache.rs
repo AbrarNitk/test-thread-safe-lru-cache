3aef15d8a3649233a663cf0c496ff19f7037826d
AbrarNitk <abrar.nitk@gmail.com>
2026-05-15 21:00:31 +0530

    sharded-lru: generic cache builder, and this seem to be working

--- src/sharded/cache.rs
+++ src/sharded/cache.rs
@@ -1,9 +1,9 @@
 use crate::sharded::{
     cache,
-    eviction::{EvictBuilder, Eviction, lru::Lru},
+    eviction::{Eviction, lru::Lru},
 };
 
-use std::hash::Hash;
+use std::{hash::Hash, marker::PhantomData};
 
 // implemented eviction policy
 pub enum EvictionPolicy {
@@ -25,77 +25,33 @@ where
     _phatntom: std::marker::PhantomData<(Key, Value)>,
 }
 
-impl<Key, Value, EP> Cache<Key, Value, EP>
+// Cache implmentation for the Lru Policy
+impl<Key, Value> Cache<Key, Value, Lru<Key, Value>>
 where
     Key: Send + Sync + Clone + Eq + Hash,
     Value: Send + Sync + Clone,
-    EP: Eviction<Key, Value>,
 {
-    fn new<Builder: Fn() -> EP>(total_shards: usize, shard_builder: Builder) -> Self {
-        let mut shards = Vec::with_capacity(total_shards);
-        for _ in 0..total_shards {
-            let shard = shard_builder();
-            shards.push(shard);
+    // build lru with the direct function call
+    pub fn lru(capacity: usize, shards: usize) -> Self {
+        assert!(
+            shards > 0,
+            "number of shards expected to be greater than zero"
+        );
+        let mut cache_shards = vec![];
+        let capacity_per_shard = capacity / shards + 1;
+        for _ in 0..shards {
+            let shard = Lru::new(capacity_per_shard);
+            cache_shards.push(shard);
         }
 
         Self {
-            shards,
-            _phatntom: std::marker::PhantomData,
-        }
-    }
-}
-
-/// Cache Builder
-/// Default Evict Policy: LRU
-/// Default Number of Shards: 16
-pub struct CacheBuilder<Key, Value, EP> {
-    capacity: usize,
-    shards: Option<usize>,
-    policy: Option<EvictionPolicy>,
-    // marking that we are certainly going to use the generic params
-    _phantom: std::marker::PhantomData<(Key, Value, EP)>,
-}
-
-impl<Key, Value, EP> CacheBuilder<Key, Value, EP>
-where
-    Key: Send + Sync + Clone + Eq + Hash,
-    Value: Send + Sync + Clone,
-    EP: Eviction<Key, Value>,
-{
-    pub fn new(capacity: usize) -> Self {
-        Self {
-            capacity,
-            shards: None,
-            policy: None,
-            _phantom: std::marker::PhantomData,
+            shards: cache_shards,
+            _phatntom: PhantomData,
         }
     }
 
-    pub fn with_shards(mut self, shards: usize) -> Self {
-        self.shards = Some(shards);
-        self
-    }
-
-    pub fn with_policy(mut self, policy: EvictionPolicy) -> Self {
-        self.policy = Some(policy);
-        self
-    }
-
-    pub fn build(self) -> Cache<Key, Value, EP> {
-        let total_shards = self.shards.unwrap_or(4);
-
-        create the builder in here
-        let cache: Cache<Key, Value, EP> = match self.policy {
-            Some(EvictionPolicy::Lru) | None => {
-                let shard_builder = move || Lru::<Key, Value>::new(self.capacity);
-                Cache::new(total_shards, shard_builder)
-            }
-            Some(EvictionPolicy::AsyncLru) => {
-                let shard_builder = move || Lru::new(self.capacity);
-                Cache::new(total_shards, shard_builder)
-            }
-        };
-        cache
-
+    // todo: this can be provided with compilation flag
+    pub fn lru_async() {
+        todo!()
     }
 }
